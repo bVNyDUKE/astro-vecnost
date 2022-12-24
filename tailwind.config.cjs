5dd4201cd4c93088e17f5d977c4aa6672ff01dbe
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-12-24 14:57:18 +0100

    add index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Noto Sans", ...defaultTheme.fontFamily.sans],
+        serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        "primary-dark": "#3d3d3d",
+        "primary-light": "#f2f2f2",
+        "secondary-dark": "#404040",
+        "secondary-light": "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
