d3d279c6b29438980daeabe84efb2ea4ad38976e
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-12-28 21:43:01 +0100

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,17 +8,21 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), image()],
   output: "server",
   vite: {
     build: {
-      target: ["es2020"],
+      target: ["es2020"]
     },
     optimizeDeps: {
       esbuildOptions: {
-        target: "es2020",
-      },
-    },
+        target: "es2020"
+      }
+    }
   },
+  adapter: vercel()
 });
