9314c65470b79280657f4246774b990c7e054f62
Darko Ivkov <bVNyDUKE@protonmail.com>
2023-03-25 14:59:04 +0100

    update vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
@@ -8,11 +7,13 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
