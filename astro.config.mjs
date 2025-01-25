e0c251349f46ef4ddc59d8a924f08fdb4debf639
Darko Ivkov <bVNyDUKE@protonmail.com>
2025-01-25 21:15:37 +0100

    remove ssr mode, prerender

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
+  prefetch: true,
   adapter: vercel({
     imageService: true,
   }),
