0b8beb16c1de91ac901c25c3f76801b8c925b855
Darko Ivkov <bVNyDUKE@protonmail.com>
2025-01-25 20:31:06 +0100

    update astro, tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
